f6db21bef4f3cbc7e946c5832800e9d32205898a
Leilani Tanimoto Abad <135250913+Leilanta@users.noreply.github.com>
2023-11-06 19:52:18 +0000

    Las desventuras de Cheesy y Michael Fasbender en Lisboa

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,34 +1,57 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from 'react';
+import cheesy from "/workspaces/leilanta-traffic-light-with-hooks/src/img/Imagen_27-10-23_a_las_17.47-removebg-preview.png"
+import smile from "/workspaces/leilanta-traffic-light-with-hooks/src/img/pngimg.com - mouth_smile_PNG6.png"
 
-//create your first component
+
 const Home = () => {
-	const [selectedColor, setSelectedColor] = useState ("green");
+  const [selectedColor, setSelectedColor] = useState("green");
+  const [isAnimationStopped, setIsAnimationStopped] = useState(false);
 
-//	el terniario: (selectedColor === "red") ? "glow" : "" ; es igual a:
-//	let str = ""
-//	if(selectedColor === "red"){
-// str = "glow"}
-//else ir(selectedColor)
+  const changeColor = (color) => {
+    setSelectedColor(color);
+  };
 
-	return (
-		<div className="trafficlight container justify-content-center bg-dark p-5">
-			<div
-				onClick={()=> setSelectedColor("red")} 
-				className={
-				"m-3 red light" + (selectedColor === "red" ? " glow" : "")
-				}></div>
-			<div 
-				onClick={()=> setSelectedColor("yellow")} 
-				className={
-				"m-3 yellow light" + (selectedColor === "yellow" ? " glow" : "")
-				}></div>
-			<div 
-				onClick={()=> setSelectedColor("green")} 
-				className={
-				"m-3 green light" + (selectedColor === "green" ? " glow" : "")
-			}></div>
-		</div>
-	);
+  useEffect(() => {
+    // Verifica si el semáforo está en rojo y detiene la animación
+    setIsAnimationStopped(selectedColor === "red");
+  }, [selectedColor]);
+
+  let movableClass = "movable";
+
+  if (isAnimationStopped) {
+    movableClass += " paused"; // Agrega la clase para detener la animación
+  }
+
+  return (
+    
+    <div className="background">
+        
+      <div className="trafficlight justify-content-left bg-dark p-5">
+     
+        <div
+          onClick={() => changeColor("red")}
+          className={"m-3 red light" + (selectedColor === "red" ? " glow" : "")}
+        ></div>
+        <div
+          onClick={() => changeColor("yellow")}
+          className={"m-3 yellow light" + (selectedColor === "yellow" ? " glow" : "")}
+        ></div>
+        <div
+          onClick={() => changeColor("green")}
+          className={"m-3 green light" + (selectedColor === "green" ? " glow" : "")}
+        ></div>
+      </div>
+      
+      <img className="cheesy" src={cheesy}/>
+      <a className='uwu'></a>
+      <img className="smile" src={smile}/>
+      <a className='cheesy'>🧍🏼</a>
+      <a className={movableClass}> 🛺 </a>
+      <div className="highway">
+      </div>
+      </div>
+    
+  );
 };
 
 export default Home;
